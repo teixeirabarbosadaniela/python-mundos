b4311db8366ea9c133c877f5411e3e41b105824e
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-05-13 11:01:28 -0300

    incrementos ex011

--- aula007-ex011_2025.py
+++ aula007-ex011_2025.py
@@ -2,7 +2,7 @@
 l = float(input('Qual a largura da parede, em metros?'))
 h = float(input('Qual a altura da parede?'))
 a = (l*h)
-ap = a/2
+ap = (a/2)
 
 print(f'Considerando a largura da parede {l}m e a altura {h}m, temos que a área da parede é {a:.2f}m².\n'
        f'Se cada litro de tinta pinta 2m², a quantidade para pintá-la por completo é de {ap:.2f}l.')
