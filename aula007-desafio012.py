9a8c9ebb0f5b9eaefe24d030b10a6bba93957dec
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-05-13 09:44:17 -0300

    correção ex 5

--- aula007-desafio012.py
+++
@@ -1,5 +0,0 @@
-###Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
-ppo = float(input('Qual é o preço do produto? R$')) #PreçoProdutoOriginal
-ppd = ppo-(ppo*(5/100)) #PreçoProdutocomDesconto
-print(f'O produto custava R${ppo:.2f}, na promoção com desconto de 5% ele passa a custar R${ppd:.2f}')
-
--- aula007-ex005_2025.py
+++ aula007-ex005_2025.py
@@ -1,6 +1,9 @@
 #Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
 
 n = int(input('Escreva um número inteiro:'))
-s = n+1
-a = n-1
-print(f'O número que você digitou foi {n}, seu sucessor é {s} e seu antecessor é {a}.')
+# s = n+1
+# a = n-1
+# print(f'O número que você digitou foi {n}, seu sucessor é {s} e seu antecessor é {a}.')
+
+##Também é possível fazer da seguinte maneira (apenas 1 variavel, o que economiza a memória):
+print(f'O número que você digitou foi {n}, seu sucessor é {n+1} e seu antecessor é {n-1}.')
