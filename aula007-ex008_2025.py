1eaf8021c6eef8a6f9fc22edd0eeb91db623831a
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-05-13 10:16:15 -0300

    incrementos ex008

--- aula007-ex008_2025.py
+++ aula007-ex008_2025.py
@@ -2,7 +2,11 @@
 
 n = float(input('Quantos metros tem o que você precisa medir?'))
 
-cm = n*100
-mm = n*1000
+# cm = n*100
+# mm = n*1000
 
-print(f'Você digitou "{n}m". Portanto, convertido temos {cm:.0f}cm ou {mm:.0f}mm. ')
+# print(f'Você digitou "{n}m". Portanto, convertido temos {cm:.0f}cm ou {mm:.0f}mm. ')
+
+##durante a correção, o guanabara pede pra que o exercício seja complementado com as demais medidas:
+
+print(f'Você digitou "{n:.0f}m".\nPortanto, a tabela com a escala torna-se:\n{n*0.001:.1f}km;\n{n*0.01:.0f}hm;\n{n*0.1:.0f}dam;\n{n:.0f}m;\n{n*10:.0f}dm;\n{n*100:.0f}cm e;\n{n*1000:.0f}mm.')
