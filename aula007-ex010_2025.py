01806cbbe63c29e8ab2c182dd3b1bbef3a6483e9
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-05-13 10:57:53 -0300

    incrementos ex010

--- aula007-ex010_2025.py
+++ aula007-ex010_2025.py
@@ -1,6 +1,6 @@
 #Crie um programa que leia quanto dinheiro uma pesssoa tem na carteira e mostre quantos Dólares ela pode comprar .
 #Considere US$1,00 = R$5,68
 
-n = float(input('Quanto você deseja converter para o valor do dólar de hoje?'))
-c = (n/5.68)
-print(f'O valor digitado foi R${n} e, com o câmbio valendo R$5.68 a cada 1U$, você terá: U${c:.2f}.')
+n = float(input('Quanto você deseja converter para o valor do dólar de hoje? R$'))
+c = (n/5.63) #alterar para cotação do dia
+print(f'O valor digitado foi R${n:.2f} e, com o câmbio valendo R$5.68 a cada 1U$, você terá: U${c:.2f}.Caso se interesse por outras cotações, segue:\nEUR${n/6.62:.2f}(euros);\nCAD${n/4.02:.2f}(dólar-canadense);\nGBP${n/7.44:.2f}(libra-esterlina);\nCNY${n/0.78:.2f}(yuan-chinês).')
