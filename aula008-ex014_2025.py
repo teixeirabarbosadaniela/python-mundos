cb3d5bc8445dfa25946fe6c4066290a6bf67ab57
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-05-13 11:22:58 -0300

    ex014

--- aula008-ex014_2025.py
+++ aula008-ex014_2025.py
@@ -1,1 +1,6 @@
-#aleluiaaaaa
+#Escreva um programa que converta uma temperatura em Celsius para Fahrenheit.
+
+c = float(input('Informe a temperatura em °C:'))
+f = ((9*c)/5)+32 #fórmula (não precisaria dos parentes, considerando regra de precedência)
+
+print(f'A temperatura de {c:.1f}°C corresponde a {f:.1f}°F!')
