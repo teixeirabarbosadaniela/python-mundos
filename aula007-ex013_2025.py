e43f813efb019214922ae07368b301da63f181b4
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-05-13 11:11:39 -0300

    incremento ex013

--- aula007-ex013_2025.py
+++ aula007-ex013_2025.py
@@ -1,5 +1,5 @@
-#Faça um algoritmo quew leia bo salário de um funcionário e mostre seu novo saláqrio com 15% de aumento.
-s = float(input('Qual o seu salário atual?'))
+#Faça um algoritmo que leia o salário de um funcionário e mostre seu novo saláqrio com 15% de aumento.
+s = float(input('Qual o seu salário atual? R$'))
 aumento = s + (s*15/100)
 print(f'Seu salário atual é R${s:.2f} e, após aplicado aumento de 15%, passará a ser R${aumento:.2f}.')
 
